b95059c5deb0896ce05a8c487cf131f05e2ecd53
swapnaliss <swapnalisbhargude123@gmail.com>
2023-06-11 16:20:27 +0530

    authentication done

--- server/index.js
+++ server/index.js
@@ -7,7 +7,9 @@ import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url"; 
+import authRoutes from "./routes/auth.js"
+import {register} from "./controllers/auth.js";
 
 // CONFIGURATIONS
 
@@ -34,6 +36,12 @@ const storage= multer.diskStorage({
     }
 });
 const upload = multer({storage})
+
+/* ROUTES WITH FILES */
+app.post("/auth/register" , upload.single("picture"), register);
+
+/*Routes*/
+app.use("auth", authRoutes);
 
 /* Mongoose setup */
 const PORT = process.env.PORT || 6001;
