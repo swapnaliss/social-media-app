a888ce0eeae289cdea824bd6792e2e74387668b9
swapnaliss <swapnalisbhargude123@gmail.com>
2023-06-15 21:34:40 +0530

    routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import HomePage from 'scenes/homePage';
+import LoginPage from 'scenes/loginPage';
+import ProfilePage from 'scenes/profilePage';
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element = {<LoginPage/>}/>
+      <Route path="/home" element = {<HomePage/>}/>
+      <Route path="/profile/:userId" element = {<ProfilePage/>}/>
+     </Routes>
+     </BrowserRouter>
     </div>
   );
 }
