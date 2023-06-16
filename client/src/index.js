5b2327b4db98fc22f1c51196d71e1a470f269edb
swapnaliss <swapnalisbhargude123@gmail.com>
2023-06-16 14:03:15 +0530

    done with readux toolkit installation and setup

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from "./state";
+import {configureStore} from "@reduxjs/toolkit";
+import {provider} from "react-redux";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import {persistGate} from "redux-persist/integration/react";
+
+const persistConfig = {key: "root", storage, version: 1};
+const persistedReducer = persistReducer(persistConfig, authReducer);
+const store = configureStore({
+  reducer : persistedReducer,
+  middleware: (getDefaultMiddleware) => 
+  getDefaultMiddleware({
+    serializableCheck : {
+      ignoredActions : [FLUSH, REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER],
+    }
+  })
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <provider store = {store}>
+      <persistGate loading = {null} persister = {persistStore(store)}>
     <App />
+    </persistGate>
+    </provider>
+    
   </React.StrictMode>
 );
 
